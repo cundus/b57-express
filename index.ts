3b4051a94ca0af25db6cd6175ea369b3e76776b5
Sundus <pcinta0@gmail.com>
2024-10-25 14:26:03 +0700

    implementasi rate limit

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
 import express, { Response, Request } from "express";
 import dotenv from "dotenv";
 import router from "./src/routes";
-
+import rateLimit from "express-rate-limit";
 dotenv.config();
 
 const app = express();
+
+// contoh per entri
+// const limiter = rateLimit({
+//    windowMs: 1 * 60 * 1000, // 5 minutes
+//    limit: 2, // each IP can make up to 10 requests per `windowsMs` (5 minutes)
+//    standardHeaders: true, // add the `RateLimit-*` headers to the response
+//    legacyHeaders: false, // remove the `X-RateLimit-*` headers from the response
+// });
+
+// app.use(limiter);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
